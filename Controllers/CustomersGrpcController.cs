0862f15f13fd5063cc3bd968a2b9fdd375f89739
cristinaneag <cristinaneag40@yahoo.com>
2021-12-16 21:10:45 +0200

    lab11

---
+++ Controllers/CustomersGrpcController.cs
@@ -0,0 +1,70 @@
+﻿using Grpc.Net.Client;
+using GrpcCustomersService;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Neag_Cristina_Lab2_EB.Controllers
+{
+    public class CustomersGrpcController : Controller
+    {
+        private readonly GrpcChannel channel;
+        public CustomersGrpcController()
+        {
+            channel = GrpcChannel.ForAddress("https://localhost:5001");
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var client = new CustomerService.CustomerServiceClient(channel);
+            CustomerList cust = client.GetAll(new Empty());
+            return View(cust);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                var client = new
+                CustomerService.CustomerServiceClient(channel);
+                var createdCustomer = client.Insert(customer);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var client = new
+               CustomerService.CustomerServiceClient(channel);
+            CustomerId idC = new CustomerId
+            {
+                Id = (int)id
+            };
+            Customer customer = client.Get(idC);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            client.Update(customer);
+
+            Console.WriteLine("done");
+            
+            return View(customer);
+        }
+    }
+}
